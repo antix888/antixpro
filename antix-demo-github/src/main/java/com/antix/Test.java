a93d0a7e44de723d5eb6b65b5eee178499d28997
antix888 <xycyj_love@126.com>
2017-08-19 00:22:08 +0800

    test commit git and push github successful

---
+++ antix-demo-github/src/main/java/com/antix/Test.java
@@ -0,0 +1,13 @@
+package com.antix;
+
+/**
+ * Created by xuyang on 2017/8/19.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println(" Hello World !!");
+
+    }
+}
---
+++ antix-demo-github/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World!antix-demo-github</h2>
+</body>
+</html>
